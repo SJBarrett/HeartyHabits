e36847fab4dc11c019b75f77146353e41c714b57
Samuel Barrett <sjbarrett276@gmail.com>
2017-11-18 01:28:46 +1000

    basic functionality

--- www/js/app.js
+++ www/js/app.js
@@ -11,13 +11,57 @@ angular.module('starter', ['ionic'])
     scaleRatio = window.devicePixelRatio / 3;
 
     //Create a new game instance and assign it to the 'gameArea' div
-    var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS, 'phaser-example',
-    {preload: preload, create: create, update: update, render: render});
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'gameArea', { preload: preload, create: create, update: update, render: render });
+
+    // var game = new Phaser.Game(window.innerWidth*window.devicePixelRatio, (window.innerHeight*window.devicePixelRatio) - 45 * window.devicePixelRatio, Phaser.CANVAS, 'gameArea', { preload: preload, create: create, update: update, render: render });
 
     function preload() {
-      game.load.image('background', 'assets/debug-grid-1920x1920.png');
-      game.load.image('player', 'assets/sprites/phaser-dude.png')''
+
+        game.load.image('background','assets/debug-grid-1920x1920.png');
+        game.load.image('player','assets/sprites/phaser-dude.png');
+
     }
+
+    var player;
+    var cursors;
+    var clickX;
+    var clickY;
+
+    function create() {
+
+        game.add.tileSprite(0, 0, 1920, 1920, 'background');
+        game.world.setBounds(0, 0, 1920, 1920);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        game.physics.arcade.enable(player);
+        player.body.fixedRotation = true;
+        cursors = game.input.keyboard.createCursorKeys();
+        //  Notice that the sprite doesn't have any momentum at all,
+        //  it's all just set by the camera follow type.
+        //  0.1 is the amount of linear interpolation to use.
+        //  The smaller the value, the smooth the camera (and the longer it takes to catch up)
+        game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+
+        game.input.onDown.add(movePlayer, this);
+    }
+
+    function movePlayer(){
+      game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+      game.physics.arcade.moveToXY(player, game.input.activePointer.x, game.input.activePointer.y, 100);
+    }
+
+    function update() {
+      if(game.physics.arcade.distanceToPointer(player) < 50){
+        player.body.velocity.setTo(0,0);
+      }
+    }
+
+    function render() {
+
+        game.debug.cameraInfo(game.camera, 32, 32);
+
+    }
+
 
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
