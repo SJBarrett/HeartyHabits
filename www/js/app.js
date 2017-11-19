7c6ec5a2a40a3b29655aa8ba909c31122192aea6
Samuel Barrett <sjbarrett276@gmail.com>
2017-11-19 11:40:58 +1000

    Added platformer assets.

--- www/js/app.js
+++ www/js/app.js
@@ -28,12 +28,14 @@ angular.module('starter', ['ionic', 'firebase', 'ngGeolocation'])
 
     function preload() {
 
-        game.load.image('background','img/grass.png');
+        game.load.image('background','assets/platformerMap.png');
         // game.load.image('player','assets/sprites/phaser-dude.png');
 
-        this.load.spritesheet('gameSprite', 'img/roguelikeSheet_transparent.png', 16, 16, 1736, 0, 1);
-        this.load.spritesheet('player', 'img/george.png', 48, 48, 16);
+        // this.load.spritesheet('player', 'img/george.png', 48, 48, 16);
         this.load.spritesheet('bee', 'img/bee.png', 60, 65, 11);
+        this.load.spritesheet('cakethulhu', 'assets/Cakethulhu.png', 128, 141);
+
+        this.load.spritesheet('player', 'assets/platformer/spritesheet.png', 72, 97, 11);
     }
 
     var player;
@@ -46,37 +48,39 @@ angular.module('starter', ['ionic', 'firebase', 'ngGeolocation'])
 
     function create() {
 
-        game.add.tileSprite(0, 0, 1920, 1920, 'background');
-        game.world.setBounds(0, 0, 1920, 1920);
+        game.add.tileSprite(0, 210, 5250, 1050, 'background');
+        game.world.setBounds(0, 0, 5250, 1050);
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.scale.setTo(0.5,0.5);
+        game.stage.backgroundColor = "#87b5ff";
 
 
-        player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+
+        player = game.add.sprite(game.world.centerX, 700, 'player');
         player.anchor.setTo(0.5, 0.5);
         player.enableBody = true;
         player.collideWorldBounds = true;
         this.physics.enable(player, Phaser.Physics.ARCADE);
 
-        randomBeePosX = Math.random() * (game.world.centerX + 300 - (game.world.centerX-300)) + (game.world.centerX-300);
-        randomBeePosY = Math.random() * (game.world.centerY + 300 - (game.world.centerY-300)) + (game.world.centerY-300);
-
-        bee = game.add.sprite(randomBeePosX, randomBeePosY, 'bee');
+        bee = game.add.sprite(player.x + 225, player.y, 'cakethulhu');
         bee.anchor.setTo(0.5, 0.5);
         bee.enableBody = true;
         bee.collideWorldBounds = true;
         this.physics.enable(bee, Phaser.Physics.ARCADE);
 
-        this.walk = player.animations.add('walk', [0, 4, 8, 12]);
-        this.left = player.animations.add('left', [1, 5, 9, 13]);
-        this.up = player.animations.add('up', [2, 6, 10, 14]);
-        this.right = player.animations.add('right', [3, 7, 11, 15]);
-        this.still = player.animations.add('still', [2]);
+        this.walk = player.animations.add('walk', [0,1,2,3,4,5,6,7,8,9,10]);
+        // this.left = player.animations.add('left', [1, 5, 9, 13]);
+        // this.up = player.animations.add('up', [2, 6, 10, 14]);
+        // this.right = player.animations.add('right', [3, 7, 11, 15]);
+        // this.still = player.animations.add('still', [2]);
 
-        this.walkBee = bee.animations.add('walkBee', [0, 1, 2]);
-        this.leftBee = bee.animations.add('leftBee', [3, 4, 5]);
-        this.rightBee = bee.animations.add('rightBee', [6, 7, 8]);
-        this.upBee = bee.animations.add('upBee', [9, 10, 11]);
-        this.stillBee = bee.animations.add('stillBee', [0]);
+        // this.walkBee = bee.animations.add('walkBee', [0, 1, 2]);
+        // this.leftBee = bee.animations.add('leftBee', [3, 4, 5]);
+        // this.rightBee = bee.animations.add('rightBee', [6, 7, 8]);
+        // this.upBee = bee.animations.add('upBee', [9, 10, 11]);
+        // this.stillBee = bee.animations.add('stillBee', [0]);
+
+
 
         game.physics.arcade.enable(player);
         player.body.fixedRotation = true;
@@ -86,7 +90,6 @@ angular.module('starter', ['ionic', 'firebase', 'ngGeolocation'])
         //  0.1 is the amount of linear interpolation to use.
         //  The smaller the value, the smooth the camera (and the longer it takes to catch up)
         game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
-
         moveEveryone();
         $rootScope.canbattle = 1
     }
@@ -133,7 +136,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngGeolocation'])
       if(game.input.activePointer.isDown && inBattle == false){
         clickX = game.input.activePointer.positionDown.x + game.camera.x;
         clickY = game.input.activePointer.positionDown.y + game.camera.y;
-        game.physics.arcade.moveToXY(player, clickX, clickY, 100);
+        game.physics.arcade.moveToXY(player, clickX, player.y, 100);
         var distX = clickX - player.position.x;
         var distY = clickY - player.position.y;
         // moving left or right
@@ -172,21 +175,21 @@ angular.module('starter', ['ionic', 'firebase', 'ngGeolocation'])
           bee.x += 0.2;
           bee.animations.play('rightBee', 3);
         }else if($rootScope.moveNum == 3){
-          bee.y += 0.2;
+          bee.y = player.y + Math.random() / 5;
           bee.animations.play('walkBee', 3);
         }else if($rootScope.moveNum == 4){
-          bee.y -= 0.2;
+          bee.y = player.y - Math.random() / 5;
           bee.animations.play('upBee', 3);
         }
       }
       if(moveDirection == 0){
-        player.animations.play('up', 5);
+        player.animations.play('walk', 5);
       } else if (moveDirection == 1) {
-        player.animations.play('right', 5);
+        player.animations.play('walk', 5);
       } else if (moveDirection == 2) {
         player.animations.play('walk', 5);
       } else if (moveDirection == 3) {
-        player.animations.play('left', 5);
+        player.animations.play('walk', 5);
       }else{
         player.animations.stop(null, true);
       }
